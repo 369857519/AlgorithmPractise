d9d278ada21a4457c37c13a424ce5c33ffb842e7
qilianshan [祁连山] <qilianshan@xiaomi.com>
2016-06-25 19:17:24 +0800

    add java file
    Signed-off-by: qilianshan [祁连山] <qilianshan@xiaomi.com>

---
+++ JAVA/multiComTest/multiCom-core/src/main/java/com/multiCom/DAO/UserDAO.java
@@ -0,0 +1,19 @@
+package com.multiCom.DAO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.multiCom.entity.User;
+/**
+ * Created by qilianshan on 16/6/16.
+ */
+public class UserDAO {
+//    public void save(Connection conn,User user) throws SQLException;
+//
+//    public void update(Connection conn,Long id,User user) throws  SQLException;
+//
+//    public void delete(Connection conn,User user) throws SQLException;
+//
+//    public ResultSet get(Connection conn, User user) throws SQLException;
+}
---
+++ JAVA/multiComTest/multiCom-core/src/main/java/com/multiCom/service/CheckUserService.java
@@ -0,0 +1,7 @@
+package com.multiCom.service;
+
+/**
+ * Created by qilianshan on 16/6/16.
+ */
+public class CheckUserService {
+}
